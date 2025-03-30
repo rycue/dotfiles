29692e8e26ada28a0d921ce4cf83fed2b2764004
Rycue <151157051+rycue@users.noreply.github.com>
2025-03-30 16:03:52 +0900

    Purplify Dream cuz it was

--- RitsukiScripts/waybar/fcitx5-language.sh
+++ RitsukiScripts/waybar/fcitx5-language.sh
@@ -3,8 +3,8 @@ state=$(fcitx5-remote)
 # Debug output (optional): echo "Raw state: $state"
 
 if [ "$state" -eq 2 ]; then
-    echo "ENG"   # Custom label for Japanese input
+    echo "JPN"   # Custom label for Japanese input
 else
-    echo "JPN"   # Custom label for English input
+    echo "ENG"   # Custom label for English input
 fi
 
