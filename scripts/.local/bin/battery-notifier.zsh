fdda8d8f13fe370b71bbb0cd06da930c740bb10c
rycue <151157051+rycue@users.noreply.github.com>
2025-05-04 11:30:19 +0800

    refactor: :children_crossing: add funtionality to log battery when low or fully-charged

--- scripts/.local/bin/battery-notifier.zsh
+++ scripts/.local/bin/battery-notifier.zsh
@@ -8,14 +8,17 @@ brightness_level=$(brightnessctl get)
 if [[ "$battery_status" == "Discharging" ]]; then
     if (( battery_level <= 15 )); then
         brightnessctl set 10%
+        echo "$(date '+%a %b %e %Y %I:%M %p'): $(acpi -b | grep -o '[0-9]\+%')" >> ~/battery_log.txt
         notify-send --urgency=critical "Critical Battery" "\nBattery level is at ${battery_level}%!" -t 15000
         sleep 5
         brightnessctl set $brightness_level
     elif (( battery_level <= 30 )); then
+        echo "$(date '+%a %b %e %Y %I:%M %p'): $(acpi -b | grep -o '[0-9]\+%')" >> ~/battery_log.txt
         notify-send --urgency=normal --category=225w "🔋 Low Battery" "Battery level is at ${battery_level}%." -t 15000
     fi
 elif [[ "$battery_status" == "Charging" ]]; then
     if (( battery_level >= 90 )); then
+        echo "$(date '+%a %b %e %Y %I:%M %p'): $(acpi -b | grep -o '[0-9]\+%')" >> ~/battery_log.txt
         notify-send --urgency=normal --category=275w "Battery level is at ${battery_level}%" "Consider unplugging the charger." -t 15000
     fi
 fi
